3b50d73e7647861b0ef080f1ad6de60e06ffa351
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-04-12 08:42:11 +0100

    Ensure all libs for c++ are present

---
+++ src/all_cpp_lib_check_test/main.cpp
@@ -0,0 +1,34 @@
+// #include "tbb/task_scheduler_init.h" // tbb
+// #include "boost/hana/basic_tuple.hpp" // boost
+// #include "Poco/File.h"
+// #include <nexus>
+// #include <hdf> // Hdf4
+// #include <hdf5>
+// #include <jemalloc>
+// #include <gsl>
+// #include <jsoncpp>
+// #include <kafka>
+// #include <qt>
+// #include <qtwebkit>
+// #include <qscintilla>
+
+// int main() {
+//     tbb::task_scheduler_init init;
+//     constexpr auto zs = boost::hana::make_basic_tuple(1, '2', 3.3);
+//     const auto file = Poco::File();
+// }
+int main() {}
+
+// tbb
+// boost
+// poco
+// nexus
+// hdf5
+// hdf4
+// jemalloc
+// gsl
+// jsoncpp
+// kafka
+// qt5
+// qtwebkit
+// qscintilla
